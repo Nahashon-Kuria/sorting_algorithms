ff86a3d55114b803d02ebee312519ca1dcbbf657
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-07-18 10:00:59 +0300

    quick sort action - check

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,83 +1,73 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * quick_sort_hoare - sorts an array of integers in ascending
- * order using the Quick sort Hoare algorithm
- *
- * @array: array
- * @size: size
+ * swap_items - Swaps two items in an array.
+ * @array: The array to modify.
+ * @l: The index of the left item.
+ * @r: The index of the right item.
  */
-void quick_sort_hoare(int *array, size_t size)
+void swap_items(int *array, size_t l, size_t r)
 {
-	if (array == NULL || size < 2)
-		return;
-	algorithm_quick_sort_h(array, 0, size - 1, size, size);
-}
+	int tmp;
 
-/**
- * algorithm_quick_sort_h - sorts an array of integers in ascending
- * order using the Quick sort Hoare algorithm
- *
- * @array: array
- * @lo: lower limit
- * @hi: upper limit
- * @size: size
- * @p: partition
- */
-void algorithm_quick_sort_h(int *array, size_t lo, size_t hi, size_t size,
-		size_t p)
-{
-	size_t new = 0;
-
-	if (lo < hi)
-	{	new = partition_h(array, lo, hi, size);
-		if (p == new)
-			p = new - 1;
-		else
-			p = new;
-		algorithm_quick_sort_h(array, lo, p, size, p);
-		algorithm_quick_sort_h(array, p + 1, hi, size, p);
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
 	}
 }
 
 /**
- * partition_h - partition function
- * order using the Quick sort algorithm
- *
- * @array: array
- * @lo: lower limit
- * @hi: upper limit
- * @size: size
- * Return: partition index
+ * quick_sort_range_hoare - Sorts a sub array using the
+ * quick sort algorithm and Hoare's partition scheme.
+ * @array: The array containing the sub-array.
+ * @low: The starting position of the sub-array.
+ * @high: The ending position of the sub-array.
+ * @size: The length of the array.
  */
-size_t	partition_h(int *array, size_t lo, size_t hi, size_t size)
+void quick_sort_range_hoare(int *array, size_t low, size_t high, size_t size)
 {
-	size_t i = 0, j = 0;
-	int pivot = 0, temp = 0, aux = 0;
+	size_t i, j;
+	int pivot;
 
-	pivot = array[hi];
-	i = lo - 1;
-	j = hi + 1;
-	while (1)
+	if ((low >= high) || (array == NULL))
+		return;
+	pivot = array[high];
+	i = low - 1;
+	j = high + 1;
+	while (TRUE)
 	{
 		do {
-			i = i + 1;
+			i++;
 		} while (array[i] < pivot);
-
 		do {
-			j = j - 1;
+			j--;
 		} while (array[j] > pivot);
+		if (i < j)
+		{
+			swap_items(array, i, j);
+			print_array(array, size);
+		}
+		else
+		{
+			break;
+		}
+	}
+	quick_sort_range_hoare(array, low, j - (j == high), size);
+	quick_sort_range_hoare(array, j - (j == high) + 1, high, size);
+}
 
-		if (i >= j)
-			return (j);
-
-		/* swap A[i] with A[j] */
-		aux = array[i];
-		temp = array[j];
-		array[j] = aux;
-		array[i] = temp;
-		print_array(array, size);
-
+/**
+ * quick_sort_hoare - Sorts an array using the quick sort algorithm
+ * and Hoare's partition scheme.
+ * @array: The array to sort.
+ * @size: The length of the array.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if ((array != NULL) && (size > 1))
+	{
+		quick_sort_range_hoare(array, 0, size - 1, size);
 	}
 }
